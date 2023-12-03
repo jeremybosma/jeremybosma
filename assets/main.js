c771c9475578ac1556a1a26e9471227a2957c670
Jeremy Bosma <jectaunscripted@gmail.com>
2023-12-03 01:38:15 +0100

    major front/backend changes

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,33 @@
 var toggles = document.querySelectorAll(".theme-toggle");
 
-var storedTheme =
-  localStorage.getItem("theme") ||
-  (window.matchMedia("(prefers-color-scheme: dark)").matches
+var storedTheme = localStorage.getItem("theme");
+
+if (!storedTheme) {
+  storedTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
     ? "dark"
-    : "light");
-if (storedTheme)
-  document.documentElement.setAttribute("data-theme", storedTheme);
+    : "light";
+}
+
+document.documentElement.setAttribute("data-theme", storedTheme);
 
 toggles.forEach(function(toggle) {
-  toggle.onclick = function () {
+  if (storedTheme === "light") {
+    toggle.innerHTML = "<button>🌙</button>";
+  } else {
+    toggle.innerHTML = "<button>☀️</button>";
+  }
+});
+
+toggles.forEach(function(toggle) {
+  toggle.onclick = function() {
     var currentTheme = document.documentElement.getAttribute("data-theme");
     var targetTheme = "light";
 
     if (currentTheme === "light") {
       targetTheme = "dark";
+      toggle.innerHTML = "<button>☀️</button>";
+    } else {
+      toggle.innerHTML = "<button>🌙</button>";
     }
 
     document.documentElement.setAttribute("data-theme", targetTheme);
