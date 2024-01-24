338ec5e5dc366b03cd35be85265f2bfa24af01c8
Jeremy Bosma <jectaunscripted@gmail.com>
2024-01-24 11:56:56 +0100

    Update global CSS and metadata in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,34 @@
 import type { Metadata } from "next";
+import { meta } from '../../next-seo.config';
 import "./globals.css";
 
 export const metadata: Metadata = {
-  icons: "/images/pfp.jpg",
-  title: "Jeremy Bosma",
-  description: "16 jarige in software, ontwerp en startups.  ",
+  metadataBase: new URL('https://jeremybosma.nl' || 'http://localhost:3000'),
+  title: meta.title,
+  description: meta.description,
+  openGraph: {
+    locale: meta.og.locale,
+    type: 'website',
+    title: meta.og.title,
+    description: meta.og.description,
+    images: meta.og.image,
+    url: meta.url,
+    siteName: meta.og.siteName,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: [{
+      url: meta.twitter.image,
+      width: meta.twitter.imageWidth,
+      height: meta.twitter.imageHeight,
+    }],
+    site: meta.twitter.domain,
+    title: meta.twitter.title,
+    description: meta.twitter.description,
+  },
+  icons: [
+    { rel: "icon", url: meta.icons },
+  ],
 };
 
 export default function RootLayout({
