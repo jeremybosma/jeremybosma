de3a80b2da4836dc006016281c405249865b6d21
Jeremy Bosma <jectaunscripted@gmail.com>
2024-02-03 17:20:15 +0100

    Update .gitignore and About section, delete images

--- src/_components/sections/About/About.tsx
+++ src/_components/sections/About/About.tsx
@@ -4,9 +4,9 @@ export default function About() {
     return (
         <section className="about" id="about">
             <h1 className='title'>Over mij</h1>
-            <h1>Mijn naam is Jeremy Bosma, ik ben 16 jaar oud, ik ben woonachtig in Nederland en ik <b>studeer</b> software-development aan het Alfa-college in Groningen.</h1>
-            <h1>Ik maak <b>websites</b> en <b>software</b> voor meerdere platformen en ik ben een zelf geleerde <b>designer</b>. Ik gebruik daarvoor <b>tools</b> als Figma, Github en Visual Studio code.</h1>
-            <h1>Ook ben ik bezig met mijn eigen <b><a href='#projects'>projecten</a></b> waarmee ik meer als 1000+ mensen bereik en heb ik mijn eigen <b>agency</b> om mij in te huren.</h1>
+            <h1>Mijn naam is Jeremy Bosma, ik ben 16 jaar oud en ik woon in Nederland. Ik ben gepassioneerd over software-ontwikkeling en studeer het aan het Alfa-college in Groningen.</h1>
+            <h1>Ik creëer prachtige websites en software voor verschillende platformen. Daarnaast ben ik een zelf geleerde designer en maak ik gebruik van tools zoals Figma, Github en Visual Studio Code.</h1>
+            <h1>Naast mijn studie werk ik aan mijn eigen <a href='#projects'>projecten</a>, waarmee ik al meer dan 1000 mensen heb kunnen bereiken. Ik heb ook mijn eigen agency, waar je mij kunt inschakelen voor jouw eigen product.</h1>
         </section>
     );
 };
--- src/_components/sections/Projects/Projects.tsx
+++ src/_components/sections/Projects/Projects.tsx
@@ -23,10 +23,10 @@ export default function Projects() {
                 />
 
                 <ProjectCard
-                    name='Coplanner'
-                    desc='Revolutionizing the way vessels operate.'
-                    icon='/images/coplanner.png'
-                    href='https://coplanner.vercel.app'
+                    name='vessels.pro'
+                    desc='Vessels.pro is a modern fleet management platform.'
+                    icon='/images/vesselspro.png'
+                    href='https://vessels.pro'
                     backgroundColor='#121212'
                 />
 
@@ -41,6 +41,8 @@ export default function Projects() {
             <details>
                 <summary className='title'>School Opdrachten</summary><br />
                 <section className="projects">
+                    <ProjectCard name='Vier op een rij (Webgame)' backgroundColor='#A64D23' href='https://webgame.jeremybosma.nl' />
+                    <ProjectCard name='Seriousapps Project' backgroundColor='#A745D3' href='https://seriousapps-project.vercel.app' />
                     <ProjectCard name='Rekenapp in PHP' backgroundColor='#A7C7E7' href='https://rekenapp.jeremybosma.nl' />
                     <ProjectCard name='Irritante Webpage' backgroundColor='#FAA0A0' href='https://irritante-webpage.jeremybosma.nl/' />
                     <ProjectCard name='Portfolio mockups in Figma' backgroundColor='#222222' href='https://www.figma.com/proto/deejQtKzKBApU6uIYjmVX4/02_did%2Fwsh-portfolio-1.0?page-id=0%3A1&type=design&node-id=1-3&viewport=557%2C445%2C0.22&t=8chiAav0CLI1frG3-1&scaling=min-zoom&starting-point-node-id=1%3A3&mode=design' />
