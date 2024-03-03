080ea85aa510b5b66e8d3cbc079e87dc3a737753
Jeremy Bosma <jectaunscripted@gmail.com>
2024-03-03 21:42:42 +0100

    Fix typos and update padding in School and globals.css

--- src/_components/sections/School/School.tsx
+++ src/_components/sections/School/School.tsx
@@ -2,7 +2,7 @@ export default function School() {
     return (
         <section className="about" id='school'>
             <h1 className='title'>School</h1>
-            <h1>Ik studeer momenteel software development aan het <a href="https://www.alfa-college.nl/" target="_blank">Alfa-College</a> in Groningen en leer ik mezelf UI/UX Design in Figma. Hiervoor het ik VMBO-TL afgestudeerd aah het <a href="https://www.campuseemsdeltagroningen.nl/" target="_blank">Campus Eemsdelta</a> in Appingedam.</h1>
+            <h1>Ik studeer momenteel software development aan het <a href="https://www.alfa-college.nl/" target="_blank">Alfa-College</a> in Groningen en leer ik mezelf UI/UX Design in Figma. Hiervoor het ik VMBO-TL afgestudeerd op het <a href="https://www.campuseemsdeltagroningen.nl/" target="_blank">Campus Eemsdelta</a> in Appingedam.</h1>
         </section>
     );
 };
