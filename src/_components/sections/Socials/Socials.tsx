228504af1e1d685842c642a414a0ae21f6e76623
Jeremy Bosma <jectaunscripted@gmail.com>
2024-03-03 19:18:51 +0100

    Update social media links in Socials component

--- src/_components/sections/Socials/Socials.tsx
+++ src/_components/sections/Socials/Socials.tsx
@@ -6,10 +6,11 @@ export default function Socials() {
         <div className='titlediv'>
             <h1 className='title'>Contact & Socials</h1>
             <section className="projects" id='contact'>
-                <ProjectCard name='@Jecta2' backgroundColor='#000' icon='/images/x.png' href='https://x.com/jecta2' />
+                <ProjectCard name='Jecta2' backgroundColor='#000' icon='/images/x.png' href='https://x.com/jecta2' />
                 <ProjectCard name='Jecta' backgroundColor='#333' icon='/images/github.png' href='https://github.com/jecta' />
-                <ProjectCard name='Donate' backgroundColor='#11C3FF' icon='/images/kofi.png' href='https://ko-fi.com/jecta' />
+                <ProjectCard name='jeremybosma_' backgroundColor='#bc2a8d' icon='/images/instagram.png' href='https://instagram.com/jeremybosma_' />
                 <ProjectCard name='Jecta' backgroundColor='#5865F2' icon='/images/discord.png' href='https://discord.com/channels/@me/709441303351394314/' />
+                <ProjectCard name='Donate' backgroundColor='#11C3FF' icon='/images/kofi.png' href='https://ko-fi.com/jecta' />
             </section>
         </div>
     );
