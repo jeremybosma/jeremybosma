edc28fbf9e1bdd1258f68696ade26211baebd891
Jeremy Bosma <jectaunscripted@gmail.com>
2024-03-03 19:20:01 +0100

    Update project card links in Projects component

--- src/_components/sections/Projects/Projects.tsx
+++ src/_components/sections/Projects/Projects.tsx
@@ -41,11 +41,11 @@ export default function Projects() {
             <details>
                 <summary className='title'>School Opdrachten</summary><br />
                 <section className="projects">
-                    <ProjectCard name='Vier op een rij (Webgame)' desc='Een webgame van Vier op een rij' backgroundColor='#A64D23' href='https://webgame.jeremybosma.nl' />
-                    <ProjectCard name='Seriousapps Project' desc='Een project van Seriousapps' backgroundColor='#A745D3' href='https://seriousapps-project.vercel.app' />
-                    <ProjectCard name='Rekenapp in PHP' desc='Een rekenapplicatie in PHP' backgroundColor='#A7C7E7' href='https://rekenapp.jeremybosma.nl' />
-                    <ProjectCard name='Irritante Webpage' desc='Een irritante webpagina' backgroundColor='#FAA0A0' href='https://irritante-webpage.jeremybosma.nl/' />
-                    <ProjectCard name='Portfolio mockups in Figma' desc='Mockups van mijn portfolio in Figma' backgroundColor='#222222' href='https://www.figma.com/proto/deejQtKzKBApU6uIYjmVX4/02_did%2Fwsh-portfolio-1.0?page-id=0%3A1&type=design&node-id=1-3&viewport=557%2C445%2C0.22&t=8chiAav0CLI1frG3-1&scaling=min-zoom&starting-point-node-id=1%3A3&mode=design' />
+                    <ProjectCard name='Vier op een rij (Webgame)' backgroundColor='#A64D23' href='https://webgame.jeremybosma.nl' />
+                    <ProjectCard name='Seriousapps Project' backgroundColor='#A745D3' href='https://seriousapps-project.vercel.app' />
+                    <ProjectCard name='Rekenapp in PHP' backgroundColor='#A7C7E7' href='https://rekenapp.jeremybosma.nl' />
+                    <ProjectCard name='Irritante Webpage' backgroundColor='#FAA0A0' href='https://irritante-webpage.jeremybosma.nl/' />
+                    <ProjectCard name='Portfolio mockups in Figma' backgroundColor='#222222' href='https://www.figma.com/proto/deejQtKzKBApU6uIYjmVX4/02_did%2Fwsh-portfolio-1.0?page-id=0%3A1&type=design&node-id=1-3&viewport=557%2C445%2C0.22&t=8chiAav0CLI1frG3-1&scaling=min-zoom&starting-point-node-id=1%3A3&mode=design' />
                 </section>
             </details>
         </div>
