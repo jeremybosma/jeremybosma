d1213ee62e8e064a3d9ea7aee0b79c0e2008fa62
Jeremy Bosma <jectaunscripted@gmail.com>
2024-03-20 10:20:34 +0100

    Update social media links in Socials component

--- src/_components/sections/Socials/Socials.tsx
+++ src/_components/sections/Socials/Socials.tsx
@@ -6,11 +6,11 @@ export default function Socials() {
         <div className='titlediv'>
             <h1 className='title'>Contact & Socials</h1>
             <section className="projects" id='contact'>
-                <ProjectCard name='Jecta2' backgroundColor='#000' icon='/images/x.png' href='https://x.com/jeremybosma_' />
-                <ProjectCard name='Jecta' backgroundColor='#333' icon='/images/github.png' href='https://github.com/jeremybosma' />
+                <ProjectCard name='jeremybosma_' backgroundColor='#000' icon='/images/x.png' href='https://x.com/jeremybosma_' />
+                <ProjectCard name='jeremybosma' backgroundColor='#333' icon='/images/github.png' href='https://github.com/jeremybosma' />
                 <ProjectCard name='jeremybosma_' backgroundColor='#bc2a8d' icon='/images/instagram.png' href='https://instagram.com/jeremybosma_' />
                 <ProjectCard name='Jecta' backgroundColor='#5865F2' icon='/images/discord.png' href='https://discord.com/channels/@me/709441303351394314/' />
-                <ProjectCard name='Jecta' backgroundColor='#11C3FF' icon='/images/kofi.png' href='https://ko-fi.com/jecta' />
+                <ProjectCard name='jeremybosma' backgroundColor='#11C3FF' icon='/images/kofi.png' href='https://ko-fi.com/jeremybosma' />
             </section>
         </div>
     );
