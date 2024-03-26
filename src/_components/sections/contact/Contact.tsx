3aba8c3309369486eab1e0ab5e6c1baa734c99f5
Jeremy Bosma <jectaunscripted@gmail.com>
2024-03-26 21:22:31 +0100

    Update import paths for Contact and Socials components

--- src/_components/sections/contact/Contact.tsx
+++ src/_components/sections/contact/Contact.tsx
@@ -1,4 +1,4 @@
-import '../projects/Projects.css';
+import '@/_components/sections/projects/Projects.css';
 import SocialMedia from '../../ui/SocialMedia';
 
 export default function Contact() {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Profile from '../_components/sections/profile/Profile';
-import About from '../_components/sections/about/About';
-import Projects from '../_components/sections/projects/Projects';
-import School from '../_components/sections/school/School';
-import Socials from '../_components/sections/contact/Contact';
+import Profile from '@/_components/sections/profile/Profile';
+import About from '@/_components/sections/about/About';
+import Projects from '@/_components/sections/projects/Projects';
+import School from '@/_components/sections/school/School';
+import Socials from '@/_components/sections/contact/Contact';
 
 export default function Home() {
   return (
