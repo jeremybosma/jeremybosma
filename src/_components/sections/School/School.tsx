08a09777f237389db3c054c5c53b4bfe4770bb65
Jeremy Bosma <jectaunscripted@gmail.com>
2024-03-30 22:54:45 +0100

    v1

--- src/_components/sections/School/School.tsx
+++
@@ -1,8 +0,0 @@
-export default function School() {
-    return (
-        <section className='about' id='school'>
-            <h1 className='title'>School</h1>
-            <p>Ik studeer momenteel software development aan het <a href='https://www.alfa-college.nl/' target='_blank' style={{ textDecoration: 'underline' }}>Alfa-College</a> in Groningen en leer ik mezelf UI/UX Design in Figma. Hiervoor heb ik VMBO-TL afgestudeerd op het <a href='https://www.campuseemsdeltagroningen.nl/' target='_blank' style={{ textDecoration: 'underline' }}>Campus Eemsdelta</a> in Appingedam.</p>
-        </section>
-    );
-};
