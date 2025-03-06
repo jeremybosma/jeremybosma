8697742469b22ae1b739168dd48d3192af1ba136
Jeremy Bosma <jectaunscripted@gmail.com>
2025-03-07 00:13:46 +0100

    new nextjs portfolio

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
-[![portfolio](https://raw.githubusercontent.com/jeremybosma/jeremybosma/refs/heads/main/public/images/ogimage.png)](https://jeremybosma.nl)
-<!-- [![x](https://github.com/Jecta/jecta/assets/68130184/2c635958-af7e-4f93-a06d-3944e1de0fbd)](https://x.com/jeremybosma_) -->
-<!-- [![ko-fi](https://github.com/Jecta/jecta/assets/68130184/87ff540c-ec4e-465b-b7be-d1272531f140)](https://ko-fi.com/jeremybosma) -->
+This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
-<!-- [learn more @ jecta.me](https://jecta.me) -->
-<!-- ![portfolio](jecta.png) -->
-<!-- !- 🌱 I’m currently studying software development and self-learning design. -->
-<!-- !- 📫 How to reach me: [contacts information](https://jeremybosma.nl/contact). -->
+## Getting Started
 
-<!-- !- [Learn more about me and what i do on my portfolio!](https://jeremybosma.nl) -->
+First, run the development server:
 
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+
+This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
---
+++ src/app/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import Image, { type StaticImageData } from "next/image";
+import Link from "next/link";
+
+import { motion } from 'motion/react'
+
+import vesselspro from "../../public/projects/vesselspro.png";
+import outfitsbio from "../../public/projects/outfitsbio.png";
+import fulldev from "../../public/projects/fulldev.png";
+
+const VARIANTS_CONTAINER = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.15,
+    },
+  },
+}
+
+const VARIANTS_SECTION = {
+  hidden: { opacity: 0, y: 20, filter: 'blur(8px)' },
+  visible: { opacity: 1, y: 0, filter: 'blur(0px)' },
+}
+
+const TRANSITION_SECTION = {
+  duration: 0.3,
+}
+
+export default function Home() {
+  return (
+    <motion.main
+      className="flex flex-col h-screen p-8 gap-8 max-w-3xl"
+      variants={VARIANTS_CONTAINER}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.section
+        variants={VARIANTS_SECTION}
+        transition={TRANSITION_SECTION}
+        className="text-[17px]"
+      >
+        <h1>Jeremy Bosma</h1>
+        <p className="text-white/60">Software Engineer &amp; Designer</p>
+      </motion.section>
+
+      <motion.section
+        variants={VARIANTS_SECTION}
+        transition={TRANSITION_SECTION}
+      >
+        <p>
+          I'm a software engineer with eye for design and micro-interactions and I aim to create memorable digital experiences and soon physical products like hardware and even clothing.
+        </p>
+      </motion.section>
+
+      <motion.section
+        variants={VARIANTS_SECTION}
+        transition={TRANSITION_SECTION}
+      >
+        <h2>What I'm working on</h2>
+        <ProjectCard name="vesselspro" description="a better solution to fleet management, ship maintenance, and more for privates and major shipping companies." image={vesselspro} link="https://vessels.pro" />
+        <ProjectCard name="outfitsbio" description="a platform to keep track of your clothing, share, and find outfit inspiration." image={outfitsbio} link="https://outfits.bio" />
+      </motion.section>
+
+      <motion.section
+        variants={VARIANTS_SECTION}
+        transition={TRANSITION_SECTION}
+      >
+        <h2>Work experience</h2>
+        <ProjectCard name="Internship at full.dev" description="web development agency that's also building development tools." image={fulldev} link="https://full.dev" />
+      </motion.section>
+
+      <motion.section
+        variants={VARIANTS_SECTION}
+        transition={TRANSITION_SECTION}
+      >
+        <h2>Contact</h2>
+        <p className="text-white/60">
+          I'm always looking for new opportunities and collaborations. If you have any questions or would like to get in touch, please don't hesitate to contact me on <Link className="underline" href="mailto:prive@jeremybosma.nl">prive@jeremybosma.nl</Link> or contact me on <Link className="underline" href="https://x.com/jeremybosma_">X</Link>.
+        </p>
+      </motion.section>
+    </motion.main>
+  );
+}
+
+type ProjectProps = {
+  name: string;
+  description: string;
+  image: StaticImageData;
+  link: string;
+}
+
+function ProjectCard({ name, description, image, link }: ProjectProps) {
+  return (
+    <Link href={link}>
+      <div className="flex gap-3 my-2 items-center">
+
+        <Image src={image} alt={name} className="w-fit h-fit rounded-md border-white/10 border-[0.5px]" width={30} height={30} />
+
+        <div className="flex flex-col">
+          <h3>{name}</h3>
+          <p className="text-white/60">{description}</p>
+        </div>
+
+      </div>
+    </Link>
+  );
+}
