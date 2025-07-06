c2b00e67d368642b955a2351c8486c0d9e70e8b3
Jeremy Bosma <jectaunscripted@gmail.com>
2025-07-06 19:44:58 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,7 +107,8 @@ export default function Home() {
         <h2>What I'm working on</h2>
         <ProjectCard name="Internet Engineering™" description="Software agency focused on development and design of agentic experiences with modern user interfaces." image={internetengineering} link="https://internet-engineering.com" />
         <ProjectCard name="0system" description="One OS to run your entire digital life. We integrate with the applications you use everyday so you can work smarter, not harder. Just type and get things done." image={zerosystem} link="https://0system.com" />
-        <ProjectCard name="vesselspro" description="A better solution to fleet management, ship maintenance, and more for privates and major shipping companies" image={vesselspro} link="https://vessels.pro" />
+        {/* <ProjectCard name="seavan" description="AI Automated container planning" image={seavan} link="https://seavan.app" /> */}
+        {/* <ProjectCard name="vesselspro" description="A better solution to fleet management, ship maintenance, and more for privates and major shipping companies" image={vesselspro} link="https://vessels.pro" /> */}
         <ProjectCard name="outfitsbio" description="Social platform to keep track of your clothing, share, and find outfit inspiration" image={outfitsbio} link="https://outfitsbio.com" />
       </motion.section>
 
