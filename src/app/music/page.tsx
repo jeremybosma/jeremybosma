f511ee51252012ee3f94fc7efe84f37d64ade460
Jeremy Bosma <jectaunscripted@gmail.com>
2025-09-07 23:01:28 +0200

    more songs n stuff

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from "motion/react";
 import { sectionProps } from "../ui/ClientLayout";
-// import Image from "next/image";
+import Image from "next/image";
 
 const music = [
     {
@@ -54,6 +54,12 @@ const music = [
         type: "single",
     },
     {
+        title: "Xscape (Deluxe)",
+        author: "Michael Jackson",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/c6ade3b17a4b43b9c3622f9f4e576fc6.jpg",
+        type: "album",
+    },
+    {
         title: "act ii: date @ 8",
         author: "4batz & Drake",
         image: "https://lastfm.freetls.fastly.net/i/u/500x500/1e5757789b12375655dd6a01aed4fc16.jpg",
@@ -66,17 +72,84 @@ const music = [
         type: "album",
     },
     {
-        title: "Xscape (Deluxe)",
-        author: "Michael Jackson",
-        image: "https://lastfm.freetls.fastly.net/i/u/500x500/c6ade3b17a4b43b9c3622f9f4e576fc6.jpg",
-        type: "album",
-    },
-    {
         title: "Figure It Out",
         author: "Ian",
         image: "https://lastfm.freetls.fastly.net/i/u/500x500/f10df30d391b438ff053f1d8f31328ec.jpg",
         type: "single",
     },
+    {
+        title: "NOKIA",
+        author: "Drake",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/bb5d2b034becb58b049c47a357d4c7ab.jpg",
+        type: "single",
+    },
+    {
+        title: "Donda 2",
+        author: "Kanye West",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/cee50b7f4762d19b6df5f6a62920dc85.jpg",
+        type: "album",
+    },
+    {
+        title: "HIM ALL ALONG",
+        author: "Gunna",
+        image: "https://lastfm.freetls.fastly.net/i/u/300x300/c6d4bb6eb6dc821bfd97fbbe4b6e00c3.jpg",
+        type: "single",
+    },
+    {
+        title: "Views",
+        author: "Drake",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/99e7715edfbd4c4519e3483a1779d574.jpg",
+        type: "album",
+    },
+    {
+        title: "back in the a",
+        author: "Gunna",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/ddf416e359672be8a0c31c9a663acd79.jpg",
+        type: "single",
+    },
+    {
+        title: "Scorpion",
+        author: "Drake",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/b42f8bf1987f3e1e4dc55d46d9c7bc3d.jpg",
+        type: "album",
+    },
+    {
+        title: "All To Myself,",
+        author: "Future, Metro Boomin, The Weeknd",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/6a3dd879bb20457745a12ac0872c8821.jpg",
+        type: "single",
+    },
+    {
+        title: "Draft Day",
+        author: "Drake",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/e569dbdf0ec2f74660e00f74f724ff32.jpg",
+        type: "single",
+    },
+    {
+        title: "Can't Tell Me Nothing",
+        author: "Kanye West",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/8ddd1959a2bef460a5149b3e0cf5e18a.jpg",
+        type: "single",
+    },
+    {
+        title: "today i did good",
+        author: "Gunna",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/ddf416e359672be8a0c31c9a663acd79.jpg",
+        type: "single",
+    },
+    {
+        title: "2 Mazza",
+        author: "Smiley, Drake",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/90b17c8ecfd29b65b5011bdd325de1fa.jpg",
+        type: "single",
+    },
+    {
+        title: "Blue Green Red",
+        author: "Drake",
+        image: "https://lastfm.freetls.fastly.net/i/u/500x500/d67665dd545b647a6da55370931d0791.jpg",
+        type: "single",
+        unreleased: true,
+    }
 ];
 
 // Removed itemVariants; using inline animation configs on elements
@@ -96,13 +169,16 @@ export default function Music() {
                         whileInView={{ opacity: 1, transition: { duration: 0.35, ease: [0.16, 1, 0.3, 1], delay: index * 0.08 } }}
                         viewport={{ once: false, amount: 0.2 }}
                     >
-                        <img src={music.image} alt={music.title} loading="eager" className="w-12 h-12 object-cover group-hover:scale-90 group-hover:rotate-3 transition-all duration-300" />
+                        <Image src={music.image} alt={music.title} loading="eager" className="w-12 h-12 object-cover group-hover:scale-90 group-hover:rotate-3 transition-all duration-300" width={100} height={100} />
                         <div className="flex flex-col">
                             <h2>{music.title}</h2>
                             <p className="text-sm text-muted-foreground">{music.author}</p>
                             <p className="text-sm flex text-muted-foreground/80 gap-1 items-center">
-                                {/* {music.unreleased && <span>unreleased</span>}{" "} */}
-                                {music.type.charAt(0).toUpperCase() + music.type.slice(1)}
+                                {music.unreleased && <span>Unreleased</span>}{" "}
+                                {music.unreleased
+                                    ? music.type
+                                    : music.type.charAt(0).toUpperCase() + music.type.slice(1)
+                                }
                             </p>
                         </div>
                     </motion.div>
